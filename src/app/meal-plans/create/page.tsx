82fe2961ceee7726698f103ee52cb67ef9cc9dcb
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-09-11 14:33:38 +0200

    Implement header on meal plan creation page

--- src/app/meal-plans/create/page.tsx
+++ src/app/meal-plans/create/page.tsx
@@ -1,8 +1,51 @@
+"use client"
+
+import PageHeader from "@/components/common/page-header/PageHeader"
+import { Check } from "@mui/icons-material";
+import { Box, Button } from "@mui/material";
+import { useRouter } from "next/navigation"
+
+const Actions = () => {
+  const router = useRouter();
+
+  const handleCancel = () => {
+    router.back();
+  };
+
+  const handleSave = () => {
+    // TODO: Handle Save
+    alert('WIP: SAVE!');
+  }
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <Button 
+        variant='outlined' 
+        sx={{ marginRight: '1rem' }}
+        onClick={handleCancel}
+      >
+        Cancel
+      </Button>
+      <Button 
+        variant='contained' 
+        startIcon={<Check />}
+        onClick={handleSave}
+        >
+          Save meal plan
+      </Button>
+    </Box>
+  )
+}
+
 const MealPlanCreatePage = () => {
   return (
-    <div>
-      Create Meal Plan
-    </div>
+    <main>
+      <PageHeader
+        actions={<Actions />}
+        title='New meal plan'
+        subTitle='Generate and save a new meal plan with ease.'
+      />
+    </main>
   )
 }
 
