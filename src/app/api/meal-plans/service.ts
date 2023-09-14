0e2ba22fa12d3627d1900c113ab800c6f7e85e0f
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-09-14 13:40:27 +0200

    Integrate the genearting of an answer into the api

--- src/app/api/meal-plans/service.ts
+++ src/app/api/meal-plans/service.ts
@@ -1,15 +1,19 @@
 import { NewMealPlanFormSchemaType } from "@/components/meal-plans/NewMealPlanForm";
-
+import { OpenAIConnection } from "@/lib/openai";
 export class MealPlansApiService {
   async generate(data: NewMealPlanFormSchemaType) {
     const prompt = this.buildPrompt(data);
-    // TODO: Send Prompt to OpenAI
-    // TODO: Return Answer 
-    return [{ title: 'DAY 1' }];
+  
+    const openAI = OpenAIConnection.getInstance();
+    const answer = await openAI.generateAnswer(prompt);
+
+    const result = answer?.choices?.[0]?.message?.content || "No result";
+
+    return result;
   }
 
   private buildPrompt(data: NewMealPlanFormSchemaType): string {
     // TODO: Build Prompt
-    return "OPEN API PROMPT";
+    return "Generate a meal plan on a vegan diet for 3 days and with 4 meals per day.";
   }
 }
